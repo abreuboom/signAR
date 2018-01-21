aeb464f677ed5c56212d01a39ba686a0349d6804
maxinepqk <maxinepqk@gmail.com>
2018-01-21 00:45:41 -0500

    Customised detailedVC to show each letter and corresponding sign

---
+++ CoreML in ARKit/LetterCell.swift
@@ -0,0 +1,13 @@
+//
+//  LetterCellCollectionViewCell.swift
+//  CoreML in ARKit
+//
+//  Created by Maxine Kwan on 1/20/18.
+//  Copyright © 2018 CompanyName. All rights reserved.
+//
+
+import UIKit
+
+class LetterCellCollectionViewCell: UICollectionViewCell {
+    
+}
